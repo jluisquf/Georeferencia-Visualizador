422b2680d28a0d181be3cdd9bb176eefbf5a2d0e
Luis Alfredo Pérez Mendoza <luis_zerosow7@hotmail.com>
2021-09-01 13:36:45 -0500

    Se separaron funciones del mapa de trafico

--- incidencias/src/app/map/components/mapa-trafico/mapa-trafico.component.ts
+++ incidencias/src/app/map/components/mapa-trafico/mapa-trafico.component.ts
@@ -16,6 +16,18 @@ export class MapaTraficoComponent implements AfterViewInit {
   this.mapServiceU = mapService;
   }
 
+  clearMap(m: any) {
+    for (let i in m._layers) {
+        if (m._layers[i]._path != undefined) {
+            try {
+                m.removeLayer(m._layers[i]);
+            } catch (e) {
+                console.log("problem with " + e + m._layers[i]);
+            }
+        }
+    }
+}
+
   ngOnInit(): void {
     let mapTrafico;
   }
@@ -30,7 +42,36 @@ export class MapaTraficoComponent implements AfterViewInit {
     attribution: '&copy; <a href="copyright">Openstreetmap</a>'
     }).addTo(mapTrafico);
     
-    
+    let firstpolyline;
+
+    this.mapServiceU.getTrafico().subscribe( ( data:any ) => {
+      this.clearMap(mapTrafico);
+      for (let i = 0; i < data[0].jams.length; i++) {
+          for (let j = 1; j < (data[0].jams[i].line.length - 1); j++) {
+
+              let pointA = new L.LatLng(data[0].jams[i].line[j].y, data[0].jams[i].line[j].x);
+              let pointB = new L.LatLng(data[0].jams[i].line[j + 1].y, data[0].jams[i].line[j + 1].x);
+              let pointList = [pointA, pointB];
+
+              if (data[0].jams[i].speed < 5) {
+                  firstpolyline = new L.Polyline(pointList, {
+                      color: 'red',
+                      weight: 6,
+                      opacity: 0.5,
+                      smoothFactor: 1
+                  });
+              } else {
+                  firstpolyline = new L.Polyline(pointList, {
+                      color: 'orange',
+                      weight: 6,
+                      opacity: 0.5,
+                      smoothFactor: 1
+                  });
+              }
+              firstpolyline.addTo(mapTrafico);
+          }
+      }setTimeout(function f(){console.log("out");},100); 
+  });
 
   }
 }
