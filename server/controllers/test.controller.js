98d441805de3fc8b4da2c672b4ea28743e9115f2
blackzero777 <44215329+blackzero777@users.noreply.github.com>
2021-10-12 17:09:06 -0500

    Updated test controller

--- server/controllers/test.controller.js
+++ server/controllers/test.controller.js
@@ -4,7 +4,7 @@ IncidenciasCtrl.diaTrafico = async(req, res) => {
 
     // let data = {
     //   'tiempo ': new RegExp('^2019-04-12')
-    // }
+    // /}
     const trafico = await traficodenso.find( {'tiempo ': new RegExp('2019-04-23')} ).sort({ 'tiempo ': 1 });
     res.json(trafico);
 }
