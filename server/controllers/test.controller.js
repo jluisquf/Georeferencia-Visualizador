e943f4eeb6d7f526c634d5af99110ee3620187e7
blackzero777 <luis_zerosow7@hotmail.com>
2021-10-12 16:50:15 -0500

    modificacion al test controller

--- server/controllers/test.controller.js
+++ server/controllers/test.controller.js
@@ -2,10 +2,10 @@ const traficodenso = require('../models/abrilTD');
 const IncidenciasCtrl = {};
 IncidenciasCtrl.diaTrafico = async(req, res) => {
 
-    let data = {
-      'tiempo ': new RegExp('^2019-04-12')
-    }
-    const trafico = await traficodenso.find( {'tiempo ': new RegExp('^2019-04-12')} ).sort({ 'tiempo': 1 });
+    // let data = {
+    //   'tiempo ': new RegExp('^2019-04-12')
+    // }
+    const trafico = await traficodenso.find( {'tiempo ': new RegExp('2019-04-23')} ).sort({ 'tiempo ': 1 });
     res.json(trafico);
 }
 module.exports = IncidenciasCtrl;
--- server/models/abrilTD.js
+++ server/models/abrilTD.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const EsquemaTraficoDenso = new Schema({}, { collection: 'abrilTest'});
+const EsquemaTraficoDenso = new Schema({}, { collection: 'abrilCompleto'});
 module.exports = mongoose.model('prueba', EsquemaTraficoDenso);
