af0919bc30d930a05d15c977b8cfe5429536c1c9
Yehosuah Mendoza <yemeli1996@gmail.com>
2022-01-19 21:17:27 -0600

    realizando las peticiones de clima y trafico en el mismo componente

--- incidencias/src/app/map/components/mapa-trafico-clima/mapa-trafico-clima.component.ts
+++ incidencias/src/app/map/components/mapa-trafico-clima/mapa-trafico-clima.component.ts
@@ -23,13 +23,16 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     paintLine: boolean = false;
     mapMarkers: any[];
     rango: number = 0;
+    arregloTrafico: any[];
     nombrePlayPausa: string = "play_circle";
     banderaPlayPausa: boolean = true;
     horario: string = "00:00";
     markers: any[]=[];
     tamSecciones: number = 0;
+    tamTrafico: number = 0;
     num:number;
-        
+    fechatraf;
+
     timeCtrl = new FormControl(this.horario, []);
     rangeControl = new FormControl(this.rango, [Validators.max(143), Validators.min(0)]);
 
@@ -59,7 +62,7 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     time: NgbTimeStruct = { hour: 0, minute: 2, second: 0 };
     mapServiceU: MapService;
     //TIMEPICKER
-
+    horarioTraficoDenso;
     lista: string[] = [""];//agrupa todos los lugares con incidencias
     listaClima: string[] = [""];//agrupa todos los lugares con incidencias
 
@@ -75,6 +78,15 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
             this.ajustarlinea(this.rango);
         })
 
+    }
+
+    ajustarlinea(rango: number) {
+        let horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
+        for (let w = 0; w < 143; w++) {
+            if (rango == w) {
+                this.horario = horas[w];
+            }
+        }
     }
 
     ngAfterViewInit() {
@@ -108,17 +120,40 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
         event.preventDefault();
         this.fechaConsulta = value;
         console.log(this.fechaConsulta);
+        this.fechaTrafico(this.fechaConsulta);
+        this.fechaClima(this.fechaConsulta);
     }
 
-    reproducir(event: Event) {
+    fechaTrafico(fecha) {
+        this.fechatraf="2019-04-01";
+        let tiempo = this.rango;
+
+        if (this.banderaMapa) {
+            this.mapServiceU.getAlcaldias().subscribe((data: any) => {
+                L.geoJSON(data[0]).addTo(this.map);
+                this.banderaMapa = false;
+            });
+
+            this.mapServiceU.gettraficoDenso(this.fechatraf).subscribe((data: any) => {
+                this.arregloTrafico = Object.values(data);
+                //this.tamTrafico = this.arregloTrafico[tiempo]["tiempo "][0];
+                console.log("fechaTrafico");
+                console.log(this.arregloTrafico);
+            });
+        }
     }
 
-    ajustarlinea(rango: number) {
-        let horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
-        for (let w = 0; w < 143; w++) {
-            if (rango == w) {
-                this.horario = horas[w];
-            }
+    fechaClima(fecha) {
+        let tiempo = this.rango;
+        let fechaTermos =  fecha;
+        if (this.banderaMapa) {
+            this.mapServiceU.getclima(fechaTermos).subscribe((data: any) => {
+                this.listaClima = Object.values(data);
+                this.num= this.listaClima.length;
+                this.tamSecciones = this.listaClima[0]["clima"];
+                console.log("fechaClima");
+                console.log(this.tamSecciones);
+            });
         }
     }
 
@@ -129,8 +164,10 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
             this.paintLine = true;
         }
     }
-    
 
-    
+    reproducir(event: Event) {
+        event.preventDefault();
+    }
+       
 
 }
