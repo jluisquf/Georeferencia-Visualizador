db7d205a31e9e300ba68fd6b356cf62c34e7c475
Yehosuah Mendoza <yemeli1996@gmail.com>
2022-01-25 19:20:15 -0600

    animacion solo termometros

--- incidencias/src/app/map/components/mapa-trafico-clima/mapa-trafico-clima.component.ts
+++ incidencias/src/app/map/components/mapa-trafico-clima/mapa-trafico-clima.component.ts
@@ -32,6 +32,8 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     tamTrafico: number = 0;
     num:number;
     fechatraf;
+    horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
+    marcasLineas: any;
 
     timeCtrl = new FormControl(this.horario, []);
     rangeControl = new FormControl(this.rango, [Validators.max(143), Validators.min(0)]);
@@ -80,15 +82,6 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
 
     }
 
-    ajustarlinea(rango: number) {
-        let horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
-        for (let w = 0; w < 143; w++) {
-            if (rango == w) {
-                this.horario = horas[w];
-            }
-        }
-    }
-
     ngAfterViewInit() {
 
         //Obtenemos de manera dinamica los lugares a mostrar en el input select
@@ -110,9 +103,21 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     }//FIN OnInit
 
     controlAtras(event: Event) {
+        if (this.rango > 0) {
+            this.rango -= 1;;
+            this.ajustarlinea(this.rango);
+             //that.ajustarTiempo(this.rango)
+            event.preventDefault();       
+        }
     }
 
     controlAdelante(event: Event) {
+        if (this.rango < 143) {
+            this.rango += 1;;
+            this.ajustarlinea(this.rango);
+            //that.ajustarTiempo(this.rango)
+            event.preventDefault();    
+        }
     }
 
     buscarFecha(event: Event, value) {
@@ -120,42 +125,145 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
         event.preventDefault();
         this.fechaConsulta = value;
         console.log(this.fechaConsulta);
-        this.fechaTrafico(this.fechaConsulta);
-        this.fechaClima(this.fechaConsulta);
+        this.rango = 0;
+        this.pintarTermometros(this.fechaConsulta);
+        this.fechaTraficoLineas();        
     }
 
-    fechaTrafico(fecha) {
-        this.fechatraf="2021-02-02";
-        let tiempo = this.rango;
+    reproducir(event: Event) {
+        event.preventDefault();
+        if (this.banderaPlayPausa) {
+            if (this.banderaPausa) {
+                //this.borrarClosters();
+
+            }
+            this.banderaPausa = false;
+            this.empezar();
+            this.nombrePlayPausa = 'pause';
+            this.banderaPlayPausa = false;
+        } else {
+            this.banderaPausa = true; 
+            this.nombrePlayPausa = 'play_circle';
+            this.empezar();
+            this.banderaPlayPausa = true;
+        }
+    }
+
+    ajustarlinea(rango: number) {
+        for (let w = 0; w < 143; w++) {
+            if (w == rango) {
+                this.horario = this.horas[w];
+            }
+        }
+    }
+
+    ajustarTiempo(rango: number) {
+        for (let w = 0; w < 143; w++) {
+            if (this.listaClima[rango]["hora"] == this.horas[w]) {
+                this.horario = this.horas[w];
+            }
+        }
+    }
+
+    pintarTermometros(fecha){
+        
+        let fechaTermos = fecha;
+        this.limpiar;
+
+        this.mapServiceU.getclima(fechaTermos).subscribe((data: any) => {
+            this.listaClima = Object.values(data);
+            this.num= this.listaClima.length;
+
+            if(this.num != 0){
+                this.tamSecciones = this.listaClima[0]["clima"].length;
+                //this.ajustarTiempo()
+                console.log(this.num)
+                let marker;
+                for (let i = 0; i < this.tamSecciones; i++) {
+                    let temperaturaSeccion = Number(JSON.stringify(data[0]['clima'][i].datos.main.temp))
+                    let nombreSeccion = data[0]['clima'][i].datos.name;
+                    let latitud = data[0]['clima'][i].latitud;
+                    let longitud = data[0]['clima'][i].longitud;
+
+                    if (temperaturaSeccion > 20) {
+                        marker = new L.marker([latitud, longitud], { icon: this.IconCaliente }).addTo(this.map).bindPopup(nombreSeccion + " °C: " + temperaturaSeccion);    
+                        this.markers.push(marker);               
+                    } else {
+                        if (temperaturaSeccion > 13) {
+                            marker = new L.marker([latitud, longitud], { icon: this.IconMedio }).addTo(this.map).bindPopup(nombreSeccion + " °C: " + temperaturaSeccion);                  
+                            this.markers.push(marker);
+                        }else{ 
+                        marker = new L.marker([latitud, longitud], { icon: this.IconFrio }).addTo(this.map).bindPopup(nombreSeccion + " °C: " + temperaturaSeccion);                  
+                        this.markers.push(marker);
+                        }
+                    }  
+                    
+                }
+                this.rango += 1;
+                this.activarBtn = false;
+            }else
+                alert("Lo sentimos, este día no esta registrado");
+        });
+    }
+
+    limpiar(){
+        for (let i = 0; i < this.tamSecciones; i++) {
+            this.map.removeLayer(this.markers[i]);
+        }     
+        this.markers = [];
+    }
+    
+    empezar(){
+        let datos = this.num;
+        let data = this.listaClima;
+        let hora_minuto= this.rango;
+        let that = this;
+
+        function animacionTermometros(){
+            that.limpiar();
+            let marker;
+            let pausa = that.banderaPausa;
+            that.ajustarlinea(hora_minuto);
+            //that.ajustarTiempo(hora_minuto)
+            for (let i = 0; i < (data[0]["clima"].length); i++) {
+                let temperaturaSeccion = Number(JSON.stringify(data[hora_minuto]['clima'][i].datos.main.temp))
+                let nombreSeccion = data[hora_minuto]['clima'][i].datos.name;
+                let latitud = data[hora_minuto]['clima'][i].latitud;
+                let longitud = data[hora_minuto]['clima'][i].longitud;
+
+                if (temperaturaSeccion > 18) {
+                    marker = new L.marker([latitud, longitud], { icon: that.IconCaliente }).addTo(that.map).bindPopup(nombreSeccion + " °C: " + temperaturaSeccion);    
+                    that.markers.push(marker);               
+                }  else {
+                    if(temperaturaSeccion < 12){
+                        marker = new L.marker([latitud, longitud], { icon: that.IconFrio }).addTo(that.map).bindPopup(nombreSeccion + " °C: " + temperaturaSeccion);                  
+                        that.markers.push(marker);
+                    }else{
+                        marker = new L.marker([latitud, longitud], { icon: that.IconMedio }).addTo(that.map).bindPopup(nombreSeccion + " °C: " + temperaturaSeccion);                  
+                        that.markers.push(marker);
+                    }                   
+                }
+            }
+            hora_minuto++;
+            if (hora_minuto < datos && pausa == false) {
+                
+                setTimeout(animacionTermometros, 3000);
+            }
+        }
+        animacionTermometros();
+    }
+    
+    fechaTraficoLineas() {
+        let that = this;
 
         if (this.banderaMapa) {
             this.mapServiceU.getAlcaldias().subscribe((data: any) => {
-                L.geoJSON(data[0]).addTo(this.map);
+                L.geoJSON(data[0]).addTo(that.map);
                 this.banderaMapa = false;
             });
-
-            this.mapServiceU.gettraficoDenso(this.fechatraf).subscribe((data: any) => {
-                this.arregloTrafico = Object.values(data);
-                //this.tamTrafico = this.arregloTrafico[tiempo]["tiempo "][0];
-                console.log("fechaTrafico");
-                console.log(this.arregloTrafico);
-            });
         }
     }
 
-    fechaClima(fecha) {
-        let tiempo = this.rango;
-        let fechaTermos =  fecha;
-        if (this.banderaMapa) {
-            this.mapServiceU.getclima(fechaTermos).subscribe((data: any) => {
-                this.listaClima = Object.values(data);
-                this.num= this.listaClima.length;
-                this.tamSecciones = this.listaClima[0]["clima"];
-                console.log("fechaClima");
-                console.log(this.tamSecciones);
-            });
-        }
-    }
 
     pintarLineas(event: Event) {
         if (this.paintLine) {
@@ -165,9 +273,6 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
         }
     }
 
-    reproducir(event: Event) {
-        event.preventDefault();
-    }
-       
+    //function animate(){ INICIO DE LA PELICULA
 
 }
