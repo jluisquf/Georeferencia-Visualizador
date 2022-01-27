b9623137c99035a7c18a48127f5c3708091879da
Yehosuah Mendoza <yemeli1996@gmail.com>
2022-01-26 21:37:07 -0600

    cambios en visualizaciones de animacion de mapa trafico y clima

--- incidencias/src/app/map/components/mapa-trafico-clima/mapa-trafico-clima.component.ts
+++ incidencias/src/app/map/components/mapa-trafico-clima/mapa-trafico-clima.component.ts
@@ -31,7 +31,8 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     arregloTrafico: any[];
     nombrePlayPausa: string = "play_circle";
     banderaPlayPausa: boolean = true;
-    horario: string = "00:00";
+    horarioclima: string = "00:00";
+    horariotrafico: string = "00:00";
     markers: any[]=[];
     tamSecciones: number = 0;
     tamTrafico: number = 0;
@@ -40,10 +41,12 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     marcasLineas: any;
     fechatraf;
     horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
-        
+    horasT: string[] = ["00:00", "00:05", "00:10", "00:15", "00:20", "00:25", "00:30", "00:35", "00:40", "00:45", "00:50", "00:55", "01:00", "01:05", "01:10", "01:15", "01:20", "01:25", "01:30", "01:35", "01:40", "01:45", "01:50", "01:55", "02:00", "02:05", "02:10", "02:15", "02:20", "02:25", "02:30", "02:35", "02:40", "02:45", "02:50", "02:55", "03:00", "03:05", "03:10", "03:15", "03:20", "03:25", "03:30", "03:35", "03:40", "03:45", "03:50", "03:55", "04:00", "04:05", "04:10", "04:15", "04:20", "04:25", "04:30", "04:35", "04:40", "04:45", "04:50", "04:55", "05:00", "05:05", "05:10", "05:15", "05:20", "05:25", "05:30", "05:35", "05:40", "05:45", "05:50", "05:55", "06:00", "06:05", "06:10", "06:15", "06:20", "06:25", "06:30", "06:35", "06:40", "06:45", "06:50", "06:55", "07:00", "07:05", "07:10", "07:15", "07:20", "07:25", "07:30", "07:35", "07:40", "07:45", "07:50", "07:55", "08:00", "08:10", "08:10", "08:15", "08:20", "08:25", "08:30", "08:35", "08:40", "08:45", "08:50", "08:55", "10:00", "09:05", "09:10", "09:15", "09:20", "09:25", "09:30", "09:35", "09:40", "09:45", "09:50", "09:55", "10:00", "10:05", "10:10", "10:15", "10:20", "10:25", "10:30", "10:35", "10:40", "10:45", "10:50", "10:55", "11:00", "11:05", "11:10", "11:15", "11:20", "11:25", "11:30", "11:35", "11:40", "11:45", "11:50", "11:55", "12:00", "12:05", "12:10", "12:15", "12:20", "12:25", "12:30", "12:25", "12:40", "12:45", "12:50", "12:55", "13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55", "14:00", "14:05", "14:10", "14:15", "14:20", "14:25", "14:30", "14:35", "14:40", "14:45", "14:50", "14:55", "15:00", "15:05", "15:10", "15:15", "15:20", "15:25", "15:30", "15:35", "15:40", "15:45", "15:50", "15:55", "16:00", "16:05", "16:10", "16:15", "16:20", "16:25", "16:30", "16:35", "16:40", "16:45", "16:50", "16:55", "17:00", "17:05", "17:10", "17:15", "17:20", "17:25", "17:30", "17:35", "17:40", "17:45", "17:50", "17:55", "18:00", "18:05", "18:10", "18:15", "18:20", "18:25", "18:30", "18:35", "18:40", "18:45", "18:50", "18:55", "19:00", "19:05", "19:10", "19:15", "19:20", "19:25", "19:30", "19:35", "19:40", "19:45", "19:50", "19:55", "20:00", "20:05", "20:10", "20:15", "20:20", "20:25", "20:30", "20:35", "20:40", "20:45", "20:50", "20:55", "21:00", "21:05", "21:10", "21:15", "21:20", "21:25", "21:30", "21:35", "21:40", "21:45", "21:50", "21:55", "22:00", "22:05", "22:10", "22:15", "22:20", "22:25", "22:30", "22:35", "22:40", "22:45", "22:50", "22:55", "23:00", "23:05", "23:10", "23:15", "23:20", "23:25", "23:30", "23:35", "23:40", "23:45", "23:50", "23:55"];
+            
 
-    timeCtrl = new FormControl(this.horario, []);
-    rangeControlT = new FormControl(this.rangoT, [Validators.max(143), Validators.min(0)]);
+    timeCtrl = new FormControl(this.horarioclima, []);
+    timeCtrl2 = new FormControl(this.horariotrafico, []);
+    rangeControlT = new FormControl(this.rangoT, [Validators.max(287), Validators.min(0)]);
     rangeControlC = new FormControl(this.rangoC, [Validators.max(143), Validators.min(0)]);
     
     //Iconos
@@ -114,9 +117,11 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     }//FIN OnInit
 
     controlAtras(event: Event) {
-        if (this.rangoC > 0) {
-            this.rangoC -= 1;;
+        if (this.rangoC > 0 || this.rangoT>0) {
+            this.rangoC -= 1;
+            this.rangoT -= 1;
             this.ajustarlinea(this.rangoC);
+            this.ajustarlineaTrafico(this.rangoT);
              //that.ajustarTiempo(this.rango)
             event.preventDefault();      
             this.empezar(); 
@@ -124,9 +129,11 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
     }
 
     controlAdelante(event: Event) {
-        if (this.rangoC < 143) {
-            this.rangoC += 1;;
+        if (this.rangoC < 143 || this.rangoT<287) {
+            this.rangoC += 1;
+            this.rangoT += 1;
             this.ajustarlinea(this.rangoC);
+            this.ajustarlineaTrafico(this.rangoT);
             //that.ajustarTiempo(this.rango)
             event.preventDefault();  
             this.empezar();  
@@ -138,22 +145,37 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
         event.preventDefault();
         this.fechaConsulta = value;
         console.log(this.fechaConsulta);
-        this.fechaTrafico(this.fechaConsulta);
-        this.fechaClima(this.fechaConsulta);
         this.fechaTraficoLineas();
+        if (this.opcionDatos == 'climaTrafico' ) {
+            this.fechaTrafico(this.fechaConsulta);
+            this.fechaClima(this.fechaConsulta);
+        } else if (this.opcionDatos == "clima") {
+            this.fechaClima(this.fechaConsulta);
+        } else{
+            this.fechaTrafico(this.fechaConsulta);
+        }
+
     }
 
     ajustarlinea(rango: number) {
         for (let w = 0; w < 143; w++) {
             if (this.listaClima[rango]["hora"] == this.horas[w]) {
-                this.horario = this.horas[w];
+                this.horarioclima = this.horas[w];
+            }
+        }
+    }
+
+    ajustarlineaTrafico(rango: number) {
+        for (let w = 0; w < 287; w++) {
+            if (w == rango) {
+                this.horariotrafico = this.horasT[w];
             }
         }
     }
 
     fechaTrafico(fecha) {
         this.fechatraf="2021-02-02";
-        let tiempo = this.rangoC;
+        let tiempo = this.rangoT;
 
         if (this.banderaMapa) {
             this.mapServiceU.getAlcaldias().subscribe((data: any) => {
@@ -204,6 +226,7 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
                     
                 }
                 this.rangoC += 1;
+                this.activarBtn = false;
             }else
                 alert("Lo sentimos, este día no esta registrado");
         });
@@ -291,12 +314,12 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
             this.banderaPausa = false;
             //Pintar ambos comparando check o radiobutton
             if (this.opcionDatos == 'climaTrafico' ) {
-                this.pintarClosters(this.rangoC);
+                this.pintarClosters(this.rangoT);
                 this.empezar();   
             } else if (this.opcionDatos == "clima") {
                 this.empezar();   
             } else{
-                this.pintarClosters(this.rangoC);
+                this.pintarClosters(this.rangoT);
             }
             //otro pintar clima
             //this.empezar();
@@ -333,8 +356,8 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
         function animacion() {
             // console.log('El tiempo es:' + tiempo);
             pausa = that.banderaPausa
-            that.rangoC = tiempo;
-            that.ajustarlinea(tiempo);
+            that.rangoT = tiempo;
+            that.ajustarlineaTrafico(tiempo);
 
             capaLineas.clearLayers();
             that.map.removeLayer(capaLineas);
@@ -377,7 +400,7 @@ export class MapaTraficoClimaComponent implements AfterViewInit {
                 tiempo++;
                 that.map.addLayer(markers);
                 if (tiempo < data.length && pausa == false) {
-                    setTimeout(animacion, 2000);
+                    setTimeout(animacion, 1000);
                 }
             }
         }
