0e2c1360952eadfed59d760fbcd7c4a5f312a466
CDavid07 <73976024+CDavid07@users.noreply.github.com>
2022-03-02 15:25:38 -0600

    Muestra imagenes

--- incidencias/src/app/map/components/mapa-contaminacion/mapa-contaminacion.component.ts
+++ incidencias/src/app/map/components/mapa-contaminacion/mapa-contaminacion.component.ts
@@ -24,11 +24,11 @@ export class MapaContaminacionComponent implements AfterViewInit {
   rango: number = 0;
   nombrePlayPausa: string = "play_circle";
   banderaPlayPausa: boolean = true;
-  horario: string = "00:00";
+  horario: string = "00:00:00";
   markers: any[]=[];
   tamSecciones: number = 0;
   num:number;
-  horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
+  horas: string[] = ["00:00:00", "01:00:00", "02:00:00", "03:00:00", "04:00:00", "05:00:00", "06:00:00", "07:00:00", "08:00:00",  "09:00:00", "10:00:00",  "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00","19:00:00","20:00:00", "21:00:00", "22:00:00", "23:00:00"]
       
   timeCtrl = new FormControl(this.horario, []);
   rangeControl = new FormControl(this.rango, [Validators.max(143), Validators.min(0)]);
@@ -36,7 +36,7 @@ export class MapaContaminacionComponent implements AfterViewInit {
   //Iconos
     IconNO = L.icon({
         iconUrl: '../.././assets/NO.png', 
-        iconSize: [30, 30],
+        iconSize: [50, 50],
         iconAnchor: [30, 30],
         popupAnchor: [-15, -35]
     });
@@ -62,7 +62,7 @@ export class MapaContaminacionComponent implements AfterViewInit {
         popupAnchor: [-15, -35]
     });
     
-    IcoO3 = L.icon({
+    IconO3 = L.icon({
         iconUrl: '../.././assets/O3.png',
         iconSize: [30, 30],
         iconAnchor: [30, 30],
@@ -123,7 +123,7 @@ export class MapaContaminacionComponent implements AfterViewInit {
       //Obtenemos de manera dinamica los lugares a mostrar en el input select
       
 
-      this.map = L.map('mapid').setView([19.37596, -99.07000], 11);
+      this.map = L.map('mapid').setView([19.37596, -99.07000], 10);
       //Fondo de trafico denso
 
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -155,21 +155,73 @@ export class MapaContaminacionComponent implements AfterViewInit {
     this.fechaConsulta = value;
     console.log(this.fechaConsulta);
     this.rango = 0;
+    this.estaciones() ;
     this.enpezarDatos(this.fechaConsulta);   
-    this.estaciones() 
   }
 
   enpezarDatos(fecha){
         let fechaC = fecha;
         this.mapServiceU.getContaminacion(fechaC).subscribe((data: any) => {
             this.listaContaminacion = Object.values(data);
-            console.log(this.listaContaminacion)
+            this.num= this.listaContaminacion[0]["datos"]["contaminacion"].length;
+            this.rango++;
+            for (let i = 0; i < this.num; i++) {
+                if(this.listaContaminacion[0]["datos"]["contaminacion"][i][2] == this.horas[this.rango])
+                    this.tamSecciones ++;
+            }
+            let marker;
+            for (let i = 0; i < this.tamSecciones; i++) {
+            let latitud = parseFloat(this.lista[0][this.listaContaminacion[0]["datos"]["contaminacion"][i][3]]["latitud"] )
+            let longitud = parseFloat(this.lista[0][this.listaContaminacion[0]["datos"]["contaminacion"][i][3]]["longitud"]) 
+            
+            switch(this.listaContaminacion[0]["datos"]["contaminacion"][i][4]) { 
+                case 'NO': { 
+                    marker = new L.marker([latitud, longitud], { icon: this.IconNO }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                } 
+                case 'NO2': { 
+                    marker = new L.marker([latitud, longitud], { icon: this.IconNO2 }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                }
+                case 'NOx': { 
+                    marker = new L.marker([latitud, longitud], { icon: this.IconNOx }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                } 
+                case 'O3': {
+                    marker = new L.marker([latitud, longitud], { icon: this.IconO3 }).addTo(this.map).bindPopup( " °C: " );                   
+                    break;
+                } 
+                case 'PM2.5': { 
+                    marker = new L.marker([latitud, longitud], { icon: this.IconPM25 }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                } 
+                case 'PM10': { 
+                    marker = new L.marker([latitud, longitud], { icon: this.IconPM10 }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                } 
+                case 'PMCO': {
+                    marker = new L.marker([latitud, longitud], { icon: this.IconPMCO }).addTo(this.map).bindPopup( " °C: " );                   
+                    break;
+                } 
+                case 'SO2': { 
+                    marker = new L.marker([latitud, longitud], { icon: this.IconSO2 }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                } 
+                case 'CO':{
+                    marker = new L.marker([latitud, longitud], { icon: this.IconCO }).addTo(this.map).bindPopup( " °C: " );                  
+                    break;
+                }
+                default: { 
+                   break; 
+                } 
+             } 
+            this.markers.push(marker);
+            }
         });
     }
   estaciones(){
     this.mapServiceU.getEstaciones().subscribe((data: any) => {
         this.lista = Object.values(data);
-        console.log(this.lista)
     });
   }
 
