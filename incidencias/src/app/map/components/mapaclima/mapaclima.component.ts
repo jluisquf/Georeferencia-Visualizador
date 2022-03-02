19831be530e7c33059e1a7c638f7b662b672ff25
CDavid07 <73976024+CDavid07@users.noreply.github.com>
2022-03-02 11:57:03 -0600

    Semaforo  Clima

--- incidencias/src/app/map/components/mapaclima/mapaclima.component.ts
+++ incidencias/src/app/map/components/mapaclima/mapaclima.component.ts
@@ -16,6 +16,8 @@ export class MapaclimaComponent implements AfterViewInit {
     //Variables
     fechaConsulta: string = "2021-10-01";
     banderaPausa: boolean = false;
+    public rojo: boolean = true;
+    public verde: boolean = false;
     banderaMapa: boolean = true;
     map: any;
     activarBtn = true;
@@ -29,7 +31,8 @@ export class MapaclimaComponent implements AfterViewInit {
     tamSecciones: number = 0;
     num:number;
     horas: string[] = ["00:00", "00:10", "00:20", "00:30", "00:40", "00:50", "01:00", "01:10", "01:20", "01:30", "01:40", "01:50", "02:00", "02:10", "02:20", "02:30", "02:40", "02:50", "03:00", "03:10", "03:20", "03:30", "03:40", "03:50", "04:00", "04:10", "04:20", "04:30", "04:40", "04:50", "05:00", "05:10", "05:20", "05:30", "05:40", "05:50", "06:00", "06:10", "06:20", "06:30", "06:40", "06:50", "07:00", "07:10", "07:20", "07:30", "07:40", "07:50", "08:00", "08:10", "08:20", "08:30", "08:40", "08:50", "09:00", "09:10", "09:20", "09:30", "09:40", "09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10", "11:20", "11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30", "13:40", "13:50", "14:00", "14:10", "14:20", "14:30", "14:40", "14:50", "15:00", "15:10", "15:20", "15:30", "15:40", "15:50", "16:00", "16:10", "16:20", "16:30", "16:40", "16:50", "17:00", "17:10", "17:20", "17:30", "17:40", "17:50", "18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20", "20:30", "20:40", "20:50", "21:00", "21:10", "21:20", "21:30", "21:40", "21:50", "22:00", "22:10", "22:20", "22:30", "22:40", "22:50", "23:00", "23:10", "23:20", "23:30", "23:40", "23:50"]
-        
+    posicion: number =0;
+
     timeCtrl = new FormControl(this.horario, []);
     rangeControl = new FormControl(this.rango, [Validators.max(143), Validators.min(0)]);
 
@@ -66,15 +69,14 @@ export class MapaclimaComponent implements AfterViewInit {
     //MAPA CLUSTER VISTO POR TODOS LOS METODOS DE LA CLASE
     mapClustering: any;
 
-    constructor(public mapService: MapService) {
+    constructor(public mapService: MapService, private elem: ElementRef) {
         this.mapServiceU = mapService;
         this.rangeControl.valueChanges.subscribe(value => {
             this.rango = value;
             console.log(value);
             // this.banderaPausa = true;
-            this.ajustarlinea(this.rango);
+            this.ajustarlinea(this.rango, this.posicion);
         })
-
     }
 
     ngAfterViewInit() {
@@ -104,8 +106,8 @@ export class MapaclimaComponent implements AfterViewInit {
     controlAtras(event: Event) {
         if (this.rango > 0) {
             this.rango -= 1;;
-            this.ajustarlinea(this.rango);
-             //that.ajustarTiempo(this.rango)
+            this.posicion -=1;
+            this.ajustarlinea(this.rango,this.posicion);
             event.preventDefault();      
             this.empezar(); 
         }
@@ -114,7 +116,8 @@ export class MapaclimaComponent implements AfterViewInit {
     controlAdelante(event: Event) {
         if (this.rango < 143) {
             this.rango += 1;;
-            this.ajustarlinea(this.rango);
+            this.posicion += 1;
+            this.ajustarlinea(this.rango,this.posicion);
             //that.ajustarTiempo(this.rango)
             event.preventDefault();  
             this.empezar();  
@@ -128,7 +131,18 @@ export class MapaclimaComponent implements AfterViewInit {
         console.log(this.fechaConsulta);
         this.rango = 0;
         this.pintarTermometros(this.fechaConsulta);
-        this.fechaTraficoLineas();        
+        this.fechaTraficoLineas(); 
+        this.cambio();  
+    }
+
+    cambio (){
+        if(this.rojo){
+            this.rojo = !this.rojo;
+            this.verde = !this.verde;
+        }else{
+            this.verde = true;
+            this.rojo = !this.rojo;
+        }
     }
 
     reproducir(event: Event) {
@@ -149,12 +163,21 @@ export class MapaclimaComponent implements AfterViewInit {
         }
     }
 
-    ajustarlinea(rango: number) {
-        for (let w = 0; w < 143; w++) {
-            if (this.listaClima[rango]["hora"] == this.horas[w]) {
-                this.horario = this.horas[w];
+    ajustarlinea(rango: number, posicion: number) {
+        
+            if (this.listaClima[rango]["hora"] == this.horas[posicion]) {
+                //console.log(this.listaClima[rango]["hora"] )
+                //console.log(this.horas[posicion])
+                this.horario = this.horas[posicion];
+                this.rojo = false;
+                this.verde = true;
             }
-        }
+            else{
+                this.verde = false;
+                this.rojo = true;
+                this.rango--;
+                this.horario = this.horas[posicion];
+            }
     }
 
     pintarTermometros(fecha){
@@ -167,6 +190,7 @@ export class MapaclimaComponent implements AfterViewInit {
             this.num= this.listaClima.length;
 
             if(this.num != 0){
+
                 this.tamSecciones = this.listaClima[0]["clima"].length;
                 console.log(this.tamSecciones)
                 let marker;
@@ -191,6 +215,7 @@ export class MapaclimaComponent implements AfterViewInit {
                     
                 }
                 this.rango += 1;
+                this.posicion += 1;
                 this.activarBtn = false;
             }else
                 alert("Lo sentimos, este día no esta registrado");
@@ -211,10 +236,12 @@ export class MapaclimaComponent implements AfterViewInit {
         let that = this;
 
         function animacionTermometros(){
-            that.limpiar();
             let marker;
             let pausa = that.banderaPausa;
-            that.ajustarlinea(hora_minuto);
+            that.ajustarlinea(hora_minuto,that.posicion);
+            if(hora_minuto != that.rango)
+                hora_minuto--;
+            that.limpiar();
 
             for (let i = 0; i < (data[0]["clima"].length); i++) {
                 let temperaturaSeccion = Number(JSON.stringify(data[hora_minuto]['clima'][i].datos.main.temp))
@@ -238,6 +265,7 @@ export class MapaclimaComponent implements AfterViewInit {
             hora_minuto++;
             if (hora_minuto < datos && pausa == false) {
                 that.rango++;
+                that.posicion++;
                 setTimeout(animacionTermometros, 1000);
             }
         }
